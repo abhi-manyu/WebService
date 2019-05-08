d6dcfc41f86e6d7ef1688ac763947b7b12254019
Abhi <abhim.moharana@gmail.com>
2019-05-08 18:50:31 +0530

    without using web.xml and using of @ApplicationPath and @Singleton for creating a resource object

---
+++ REST_Advanced/src/main/java/org/REST_Advanced/example/services/Company.java
@@ -0,0 +1,22 @@
+package org.REST_Advanced.example.services;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/companies")
+@Singleton
+public class Company
+{
+	private int count;
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+   public String getCompanies()
+   {
+		count++;
+	   return "it worked for "+count+" times";
+   }
+}
---
+++ REST_Advanced/src/main/java/org/REST_Advanced/example/services/MyApp.java
@@ -0,0 +1,11 @@
+package org.REST_Advanced.example.services;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("rest")
+public class MyApp extends Application
+{
+
+}
